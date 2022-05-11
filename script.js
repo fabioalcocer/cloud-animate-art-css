329709b6df360a99b788104eb53c9d247a9818db
Fabio Alcocer <fabioalcocersejas17@gmail.com>
2022-05-11 19:27:33 -0400

    set volumen

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const gif = document.querySelector('.nezuko-img')
 const audio = document.querySelector("audio");
-audio.volume = 0.25;
 
 gif.addEventListener('click', () => {
   // audio.setAttribute('autoplay', "true");
   audio.play()
+  audio.volume = 0.2;
   gif.classList.add('jump')
 })
