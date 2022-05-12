032646d4c3578af998174b2ee4615afd25e6cacb
Fabio Alcocer <fabioalcocersejas17@gmail.com>
2022-05-11 20:58:14 -0400

    add jump animation

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const gif = document.querySelector('.nezuko-img')
 const audio = document.querySelector("audio");
 
+const cloud = document.querySelector('.cloud')
+
 gif.addEventListener('click', () => {
   // audio.setAttribute('autoplay', "true");
   audio.play()
   audio.volume = 0.2;
-  gif.classList.add('jump')
+  gif.classList.toggle('animate')
 })
